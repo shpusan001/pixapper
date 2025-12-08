0ec831b856e9565fe182a5ef5357034193f1c2ee
Lee Sang Hoon <shpusan001@gmail.com>
2025-12-09 07:57:31 +0900

    Initial Commit

---
+++ Pixapper/PixapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  PixapperApp.swift
+//  Pixapper
+//
+//  Created by LeeSangHoon on 12/9/25.
+//
+
+import SwiftUI
+
+@main
+struct PixapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PixapperTests/PixapperTests.swift
@@ -0,0 +1,17 @@
+//
+//  PixapperTests.swift
+//  PixapperTests
+//
+//  Created by LeeSangHoon on 12/9/25.
+//
+
+import Testing
+@testable import Pixapper
+
+struct PixapperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
